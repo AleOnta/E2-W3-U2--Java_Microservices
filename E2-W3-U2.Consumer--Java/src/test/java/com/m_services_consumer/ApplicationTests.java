013ed6e5bbfa201384cb3d162d70e9babfa928ae
Alessandro <114835798+AleOnta@users.noreply.github.com>
2023-04-26 23:01:14 +0200

    initialized consumer project/server

---
+++ E2-W3-U2.Consumer--Java/src/test/java/com/m_services_consumer/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.m_services_consumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
