cb090374533cffeedaf11984ae0a557ea432a3cd
Alessandro <114835798+AleOnta@users.noreply.github.com>
2023-04-26 22:59:43 +0200

    initialized producer server/project

---
+++ E2-W3-U2.Producer--Java/src/main/java/com/m_services_producer/Application.java
@@ -0,0 +1,13 @@
+package com.m_services_producer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ E2-W3-U2.Producer--Java/src/test/java/com/m_services_producer/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.m_services_producer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
